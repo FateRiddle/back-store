84899bfc14400e9afce6d098578e8872d9d10c8a
FateRiddle <fateriddle@gmail.com>
2021-07-05 16:01:18 +0800

    fix 路径

--- .umirc.js
+++ .umirc.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/back-store/',
   title: 'back-store',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
